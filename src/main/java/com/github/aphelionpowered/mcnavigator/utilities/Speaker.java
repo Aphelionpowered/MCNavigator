3cc7f4f47bccc84b1b6fb1a12e84bc8320ae79e6
Mentioum <mentioummetis@gmail.com>
2012-04-09 02:08:34 +0100

    Added basic text formatting to Speaker

--- src/main/java/com/github/aphelionpowered/mcnavigator/utilities/Speaker.java
+++ src/main/java/com/github/aphelionpowered/mcnavigator/utilities/Speaker.java
@@ -1,4 +1,28 @@
 package com.github.aphelionpowered.mcnavigator.utilities;
+import java.util.logging.Logger;
+import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
+
 public class Speaker {
   
+  private static String prefix = "[Compass] " + ChatColor.WHITE;
+  private static String suffix;
+  private static Logger logger;
+
+  public static void LogInfo(String message){
+    logger.info(message);
+  }
+  
+  public static void SendSuccess(CommandSender sender, String message){
+    sender.sendMessage(ChatColor.GREEN + prefix + message);
+  }
+  
+  public static void SendError(CommandSender sender, String message){
+    sender.sendMessage(ChatColor.RED + prefix + message);
+  }
+
+  public static void denyConsole(CommandSender sender){
+    
+  }  
+
 }
