b9455d2f9f2af0d3ce7d906f84e40a3c1c26f403
Mentioum <mentioummetis@gmail.com>
2012-04-13 01:38:55 +0100

    Started work on the config manager class.

--- src/main/java/com/github/aphelionpowered/mcnavigator/utilities/ConfigMan.java
+++ src/main/java/com/github/aphelionpowered/mcnavigator/utilities/ConfigMan.java
@@ -1,5 +1,21 @@
 package com.github.aphelionpowered.mcnavigator.utilities;
+import com.github.aphelionpowered.mcnavigator.MCNavigator;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+ 
+public class ConfigMan{
 
-public class ConfigMan {
-    
+	private MCNavigator plugin;
+
+	public void loadConfiguration(MCNavigator plugin){
+		this.plugin = plugin;
+
+		
+	}
+
+
 }
+  
+	
+
